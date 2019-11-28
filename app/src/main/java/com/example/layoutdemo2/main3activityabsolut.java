b1742dc4ed9ae142bce3dabb5ad731767f057523
Sumandeep2111 <sumandeep21111994@gmail.com>
2019-11-28 14:37:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/layoutdemo2/main3activityabsolut.java
@@ -0,0 +1,14 @@
+package com.example.layoutdemo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main3activityabsolut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3activityabsolut);
+    }
+}
---
+++ app/src/main/java/com/example/layoutdemo2/relativelayout.java
@@ -0,0 +1,14 @@
+package com.example.layoutdemo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class relativelayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relativelayout);
+    }
+}
